26c4570b485e8d7cacc33c7e53a3eb0d743e667c
Furia25 <valentinledur@gmail.com>
2026-02-03 20:23:03 +0100

    feat: basic operation

--- includes/variadics.hpp
+++ includes/variadics.hpp
@@ -6,7 +6,7 @@
 /*   By: vdurand <vdurand@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 17:22:26 by vdurand           #+#    #+#             */
-/*   Updated: 2026/02/03 18:59:26 by vdurand          ###   ########.fr       */
+/*   Updated: 2026/02/03 20:22:42 by vdurand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,28 @@
 
 # include "basics.hpp"
 
-#define _MVA_ARGS_(...) __VA_ARGS__
-#define MVA_ARGS(...) _MVA_ARGS_(__VA_ARGS__)
+#define _M_VA_ARGS_(...) __VA_ARGS__
+#define M_VA_ARGS(...) _M_VA_ARGS_(__VA_ARGS__)
 
-#define _MCAT5_(_0, _1, _2, _3, _4) _0 ## _1 ## _2 ## _3 ## _4
+#define _M_CAT5_(_0, _1, _2, _3, _4) _0 ## _1 ## _2 ## _3 ## _4
 
-#define _MVA_ARGS_IDENTITY_(x, ...) x, __VA_ARGS__
-#define _MVA_ARGS_IDENTITY(x, ...) _MVA_ARGS_IDENTITY_(x, __VA_ARGS__)
+#define _M_VA_ARGS_TAIL(_0,_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14, x, ...) x
+#define _M_VA_ARGS_SEQ16() 15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0
 
-#define _MVA_ARGS_TAIL(_0,_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14, x, ...) x
-#define _MVA_ARGS_SEQ16() 15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0
+#define _M_HAS_COMMA(...) M_IDENTITY(_M_VA_ARGS_TAIL(__VA_ARGS__, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0))
 
-#define _MHAS_COMMA(...) MIDENTITY(_MVA_ARGS_TAIL(__VA_ARGS__, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0))
-#define _MIS_EMPTY_TRIGGER_PARENTHESIS(...) ,
+#define M_VA_ARGS_IS_EMPTY(...) _M_VA_ARGS_IS_EMPTY_(									\
+	_M_HAS_COMMA(__VA_ARGS__),												\
+	_M_HAS_COMMA(M_COMMA __VA_ARGS__),				\
+	_M_HAS_COMMA(__VA_ARGS__(/*EMPTY*/)),									\
+	_M_HAS_COMMA(M_COMMA __VA_ARGS__ (/*EMPTY*/)))
 
-#define MVA_ARGS_IS_EMPTY(...) _MVA_ARGS_IS_EMPTY_(									\
-	_MHAS_COMMA(__VA_ARGS__),												\
-	_MHAS_COMMA(_MIS_EMPTY_TRIGGER_PARENTHESIS __VA_ARGS__),				\
-	_MHAS_COMMA(__VA_ARGS__(/*EMPTY*/)),									\
-	_MHAS_COMMA(_MIS_EMPTY_TRIGGER_PARENTHESIS __VA_ARGS__ (/*EMPTY*/)))
-
-#define _MVA_ARGS_IS_EMPTY_(_0, _1, _2, _3) _MHAS_COMMA(_MCAT5_(_IS_EMPTY_IS_EMPTY_CASE_, _0, _1, _2, _3))
+#define _M_VA_ARGS_IS_EMPTY_(_0, _1, _2, _3) _M_HAS_COMMA(_M_CAT5_(_IS_EMPTY_IS_EMPTY_CASE_, _0, _1, _2, _3))
 #define _IS_EMPTY_IS_EMPTY_CASE_0001 ,
 
-#define _MVA_ARGS_SIZE_(...) MIDENTITY(_MVA_ARGS_TAIL(__VA_ARGS__))
-#define MVA_ARGS_SIZE(...) MIFF(MVA_ARGS_IS_EMPTY(__VA_ARGS__))(0, _MVA_ARGS_SIZE_(__VA_ARGS__, _MVA_ARGS_SEQ16()))
+#define _M_VA_ARGS_SIZE_(...) M_IDENTITY(_M_VA_ARGS_TAIL(__VA_ARGS__))
+#define M_VA_ARGS_SIZE(...) M_IFF(M_VA_ARGS_IS_EMPTY(__VA_ARGS__))(0, _M_VA_ARGS_SIZE_(__VA_ARGS__, _M_VA_ARGS_SEQ16()))
 
-#define MHEAD(x, ...) x
+#define M_HEAD(x, ...) x
 
 #endif // _MACROSPLOSION_VA_ARGS_H
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: vdurand <vdurand@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 17:01:25 by vdurand           #+#    #+#             */
-/*   Updated: 2026/02/03 18:34:15 by vdurand          ###   ########.fr       */
+/*   Updated: 2026/02/03 20:20:19 by vdurand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,26 +30,27 @@
 			throw (C2);\
 	} while(0)
 
+
 int	main(void)
 {
-	static_assert(MVA_ARGS_SIZE() == 0, "1");
-	static_assert(MVA_ARGS_SIZE(/*comment*/) == 0, "2");
-	static_assert(MVA_ARGS_SIZE(a) == 1, "3");
-	static_assert(MVA_ARGS_SIZE(a, b) == 2, "4");
-	static_assert(MVA_ARGS_SIZE(a, b, c) == 3, "5");
-	static_assert(MVA_ARGS_SIZE(a, b, c, d) == 4, "6");
-	static_assert(MVA_ARGS_SIZE(a, b, c, d, e) == 5, "7");
-	static_assert(MVA_ARGS_SIZE((void)) == 1, "8");
-	static_assert(MVA_ARGS_SIZE((void), b, c, d) == 4, "9");
-	static_assert(MVA_ARGS_SIZE(_MIS_EMPTY_TRIGGER_PARENTHESIS) == 1, "10");
-	static_assert(MVA_ARGS_SIZE(EATER0) == 1, "11");
-	static_assert(MVA_ARGS_SIZE(EATER1) == 1, "12");
-	static_assert(MVA_ARGS_SIZE(EATER2) == 1, "13");
-	static_assert(MVA_ARGS_SIZE(EATER3) == 1, "14");
-	static_assert(MVA_ARGS_SIZE(EATER4) == 1, "15");
-	static_assert(MVA_ARGS_SIZE(MAC0) == 1, "16");
+	static_assert(M_VA_ARGS_SIZE() == 0, "1");
+	static_assert(M_VA_ARGS_SIZE(/*comment*/) == 0, "2");
+	static_assert(M_VA_ARGS_SIZE(a) == 1, "3");
+	static_assert(M_VA_ARGS_SIZE(a, b) == 2, "4");
+	static_assert(M_VA_ARGS_SIZE(a, b, c) == 3, "5");
+	static_assert(M_VA_ARGS_SIZE(a, b, c, d) == 4, "6");
+	static_assert(M_VA_ARGS_SIZE(a, b, c, d, e) == 5, "7");
+	static_assert(M_VA_ARGS_SIZE((void)) == 1, "8");
+	static_assert(M_VA_ARGS_SIZE((void), b, c, d) == 4, "9");
+	static_assert(M_VA_ARGS_SIZE(_MIS_EMPTY_TRIGGER_PARENTHESIS) == 1, "10");
+	static_assert(M_VA_ARGS_SIZE(EATER0) == 1, "11");
+	static_assert(M_VA_ARGS_SIZE(EATER1) == 1, "12");
+	static_assert(M_VA_ARGS_SIZE(EATER2) == 1, "13");
+	static_assert(M_VA_ARGS_SIZE(EATER3) == 1, "14");
+	static_assert(M_VA_ARGS_SIZE(EATER4) == 1, "15");
+	static_assert(M_VA_ARGS_SIZE(MAC0) == 1, "16");
 	// a warning in msvc
-	static_assert(MVA_ARGS_SIZE(MAC1) == 1, "17");
-	static_assert(MVA_ARGS_SIZE(MACV) == 1, "18");
-	static_assert(MVA_ARGS_SIZE(MAC2(4, 5)) == 1, "19");
+	static_assert(M_VA_ARGS_SIZE(MAC1) == 1, "17");
+	static_assert(M_VA_ARGS_SIZE(MACV) == 1, "18");
+	static_assert(M_VA_ARGS_SIZE(MAC2(4, 5)) == 1, "19");
 }
